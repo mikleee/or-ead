34390166cd966e5ce45881b0d66cf587dc66aaa0
Mikhail Tkachenko <m.tkachenko@aimprosoft.com>
2016-09-28 13:15:10 +0300

    7339

--- src/main/java/uk/co/virtual1/model/xml/out/Constants.java
+++ src/main/java/uk/co/virtual1/model/xml/out/Constants.java
@@ -3,25 +3,25 @@ package uk.co.virtual1.model.xml.out;
 /**
  * @author Mikhail Tkachenko created on 22.08.16 9:49
  */
-public class Constants {
+public interface Constants {
 
-    public final static String ETHERNET_ACCESS_DIRECT_OR = "ETHERNET ACCESS DIRECT_OR";
+    String ETHERNET_ACCESS_DIRECT_OR = "ETHERNET ACCESS DIRECT_OR";
 
 
-    public final static String END_A = "A";
-    public final static String END_B = "B";
+    String END_A = "A";
+    String END_B = "B";
 
-    public final static String BLANK = "";
+    String BLANK = "";
 
-    public final static String Y = "Y";
-    public final static String N = "N";
+    String Y = "Y";
+    String N = "N";
 
-    public final static String HOUSING_CABINET = "CABINET";
+    String HOUSING_CABINET = "CABINET";
 
-    public final static String SITE_TYPE_BT = "BT";
-    public final static String SITE_TYPE_NON_BT = "Non BT";
+    String SITE_TYPE_BT = "BT";
+    String SITE_TYPE_NON_BT = "Non BT";
 
-    public final static String ADDRESS_TYPE_FULL = "Full";
+    String ADDRESS_TYPE_FULL = "Full";
 
 
 }
